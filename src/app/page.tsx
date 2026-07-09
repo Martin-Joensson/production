aacb3b8dc6715b03d486c0051285560ce05fd113
Martin Jönsson <martin.t.joensson@gmail.com>
2026-07-09 14:34:12 +0200

    Add: Product cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedProducts from "@/components/landing-page/featured-products";
 import HeroSection from "@/components/landing-page/hero-section";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
       <HeroSection />
+
+      <FeaturedProducts />
     </div>
   );
 }

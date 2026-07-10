c5d65d3650665b9e332ce1615b17cca08f4c5a3c
Martin Jönsson <martin.t.joensson@gmail.com>
2026-07-10 13:10:37 +0200

    Add: Basic footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer>Ibuilt this inc</footer>;
+  return (
+    <footer className="border-t bg-muted/20 py-12">
+      <div className="wrapper">iBuiltThis inc. All right reserved.</div>
+    </footer>
+  );
 }
